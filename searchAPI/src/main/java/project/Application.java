c1bc05b5bfff5a90d8808b80cf0691bcb0f79399
junxuan <atrocitythemetms@gmail.com>
2020-06-27 19:59:13 -0400

    serve static server

--- searchAPI/src/main/java/project/Application.java
+++ searchAPI/src/main/java/project/Application.java
@@ -6,6 +6,14 @@ public class Application {
     public static void main(String[] args) {
         String ps = System.getenv("PORT");
         int port = ps == null ? 8080 : Integer.parseInt(ps);
-        Blade.of().get("/test", ctx-> ctx.text("Hello My project")).listen(port).start();
+        Blade bld = Blade.of();
+        setUpRouter(bld);
+        bld.listen(port).start();
+    }
+
+    private static void setUpRouter(Blade bld) {
+        bld.get("/", ctx-> {
+           ctx.render("test.html");
+        });
     }
 }
