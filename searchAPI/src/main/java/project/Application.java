6213251d873e78d9d47d949d109ff8a7850a8bec
junxuan <atrocitythemetms@gmail.com>
2020-06-27 18:38:57 -0400

    test deploy

--- searchAPI/src/main/java/project/Application.java
+++ searchAPI/src/main/java/project/Application.java
@@ -4,6 +4,8 @@ import com.blade.Blade;
 
 public class Application {
     public static void main(String[] args) {
-        Blade.of().get("/test", ctx-> ctx.text("Hello My project")).listen(8080).start();
+        String ps = System.getenv("PORT");
+        int port = ps == null ? 8080 : Integer.parseInt(ps);
+        Blade.of().get("/test", ctx-> ctx.text("Hello My project")).listen(port).start();
     }
 }
