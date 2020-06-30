3f797eb4a178b9b2b23a80de562e43519eb4e889
junxuan <atrocitythemetms@gmail.com>
2020-06-30 01:53:18 -0400

    set up necessary react page components

--- client/entry_notebook/src/App.js
+++ client/entry_notebook/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import NotFoundPage from "./nofound/page404";
 function App() {
   return (
     <div className="App">
+      <NotFoundPage></NotFoundPage>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ client/entry_notebook/src/nofound/page404.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import CustomNavbar from "../common/widgets/customNavbar";
+class NotFoundPage extends React.Component {
+    constructor(props: {}) { 
+        super(props);
+    }
+
+    render() { 
+        return (
+            <CustomNavbar>
+
+            </CustomNavbar>
+        );
+    }
+}
+
+export default NotFoundPage;
