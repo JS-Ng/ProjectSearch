47118de7ca65d41275ba7f0c1527ecb2df58d90e
junxuan <atrocitythemetms@gmail.com>
2020-07-01 03:04:49 -0400

    finish 404 page

---
+++ client/entry_notebook/src/common/widgets/CustomNavbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import BootstrapComponent from "../../core/BootstrapComponent"
+import brand from "../../img/brand.jpg";
+import "./customNavbar.css";
+import {
+    Navbar, Nav,
+    NavDropdown, Form,
+    FormControl, Button, Image
+} from 'react-bootstrap';
+class CustomNavbar extends BootstrapComponent { 
+
+    render() { 
+        return (
+            <Navbar bg="light" expand="lg" className="navbar">
+                <Navbar.Brand href="#home"><Image src={brand} alt="Note" className="rounded-circle brand"></Image></Navbar.Brand>
+                <Form inline className="mx-auto">
+                    
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2 nav-search" />
+                    <Button variant="outline-success">Search</Button>
+                </Form>
+            </Navbar>
+        );
+    }
+}
+
+export default CustomNavbar;
---
+++ client/entry_notebook/src/nofound/NotFoundPage.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import CustomNavbar from "../common/widgets/CustomNavbar";
+import BootstrapComponent from "../core/BootstrapComponent";
+import "./page404.css";
+import { Container, Row, Col } from "react-bootstrap";
+import BgContainer from '../common/background/BgContainer.component';
+class NotFoundPage extends BootstrapComponent {
+   
+
+    render() { 
+        return (
+            <>
+                <CustomNavbar/>
+                <BgContainer>
+                    <Row className="row-404">
+                        <div className="col-md-12">
+                            <div className="error-template">
+                                <h1 className="">404 NOT FOUND</h1>
+                            </div>
+                            <div className="error-details">
+                                Sorry, an error has occured, Requested page not found!
+                            </div> 
+                        </div>
+                    </Row>
+                    <Row className="row-404">
+                        <Col className="custom-col">
+                            <div className="col-md-12 view overlay zoom">
+                                <div className="error-template mask flex-center">
+                                    <h2>
+                                    Back to your profile</h2>
+                                    
+                                    <div className="error-actions">
+                                        <a href="#" className="btn btn-primary btn-lg"><span className="glyphicon glyphicon-home"></span>
+                                            Profile </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </Col>
+
+                        <Col className="custom-col">
+                            <div className="col-md-12">
+                                <div className="error-template">
+                                    <h2>
+                                    Try draw a new card</h2>
+                                    <div className="error-actions">
+                                        <a href="#" className="btn btn-primary btn-lg"><span className="glyphicon glyphicon-home"></span>
+                                            Draw Card </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </BgContainer>
+            </>
+        );
+    }
+}
+
+export default NotFoundPage;
